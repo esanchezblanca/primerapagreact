853fa009155584ca0639bf2bc6224c5135db3924
Elena <esanchez6@gmail.com>
2020-11-15 18:12:43 +0100

    finalizada primera página web react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import Navigation from './components/Navigation';
+import {plantarray} from './plantarray.json';
+
+class App extends Component {
+    constructor(){
+        super(); //hereda toda la funcionalidad de React
+        this.state = {
+            plantarray
+        }
+    }
+
+    render() {
+        //recorrer el array
+        const plantarray = this.state.plantarray.map((plantarray, i) =>{
+            return(
+                <div className="card">
+                    <div className="card-header">
+                        <h3>{plantarray.name}</h3>
+                    </div>
+                    <div className="card-body">
+                    <p>
+                        {plantarray.description}
+                    </p>
+                    <img src= {plantarray.urlimg}></img>
+                    </div>
+                    
+                </div>
+            )
+        })
+            
+
+        return (
+            <div className="App">
+                
+             <nav className="navbar navbar-dark bg-dark">
+                 <a href="" className="text-white">
+                Plants
+                </a>
+
+            </nav>
+
+            {plantarray}
+            </div>
+        )
+    }
 }
 
 export default App;
